6e5024aaecc20abe8a155eb2da572a57f1b419fa
Osman Batur Ince <osmanbaturince@outlook.com>
2020-10-23 13:05:36 +0300

    master: printed the sum method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Number 1: " + num1);
         System.out.println("Number 2: " + num2);
         System.out.println("Multiplication: " + multiply(num1, num2));
+        System.out.println("Summation: " + sum(num1, num2));
     }
 
     public static int multiply(int num1, int num2){
