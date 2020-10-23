20366b50f74a503931b84cb72ae39ec4ae7a9605
Osman Batur Ince <osmanbaturince@outlook.com>
2020-10-23 12:40:27 +0300

    master: initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 500;
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+    }
+
+    public static int multiply(int num1, int num2){
+        return 0;
+    }
+
+    public static int sum(int num1, int num2){
+        return 0;
+    }
+
+    public static int subtract(int num1, int num2){
+        return 0;
+    }
+
+    public static void printHello(int name){
+        System.out.println("Hello!");
+    }
+}
